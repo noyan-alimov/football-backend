69afe032d8cb03ace67b5a4f12026723933002fb
Noyan Alimov <noyan.alimov@gmail.com>
2020-10-01 17:50:27 +0100

    added new entity

--- src/entities/Datee.ts
+++ src/entities/Datee.ts
@@ -1,5 +1,6 @@
 import { Entity, Column, PrimaryGeneratedColumn, OneToMany, ManyToOne } from "typeorm";
 import { FootballPitch } from "./FootballPitch";
+import { HourlyTime } from "./HourlyTime";
 
 @Entity()
 export class Datee {
@@ -12,4 +13,7 @@ export class Datee {
 
     @ManyToOne(type => FootballPitch, footballPitch => footballPitch.dates)
     footballPitch: FootballPitch;
+
+    @OneToMany(type => HourlyTime, hourlyTime => hourlyTime.date)
+    hourlyTimes: HourlyTime[];
 }
