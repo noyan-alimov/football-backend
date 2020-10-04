4045319f6dd7b98142cc2562e61cf469bfb6dcee
Noyan Alimov <noyan.alimov@gmail.com>
2020-10-04 14:42:01 +0100

    setup dotenv config and added one more test

--- src/controllers/footballPitch/tests/updateFootballPitch.test.ts
+++ src/controllers/footballPitch/tests/updateFootballPitch.test.ts
@@ -3,32 +3,30 @@ import { app } from "../../../index";
 import dotenv = require('dotenv');
 dotenv.config();
 
-it('should return status code 201 and football pitch data', async (done) => {
-    const req = { 
-        userId: 'authUserId', 
-        name: 'Stamford Bridge',
-        address: 'London',
-        contactNumber: '07747 237 456',
-        pricePerHour: 50  
+it('should return status code 200 and football pitch data', async (done) => {
+    const req = {
+        name: 'Old Trafford',
+        address: 'Manchester',
+        contactNumber: '06646 888 959',
+        pricePerHour: 100  
     };
     const token = process.env.TEST_TOKEN;
     const res = await request(app)
         .post('/api/football-pitch')
         .send(req)
         .set('Authorization', token);
-    expect(res.status).toBe(201);
+    expect(res.status).toBe(200);
     expect(res.body.success).toBeTruthy();
     expect(res.body.data).toBeDefined();
     done();
 });
 
 it('should return status code 401', async (done) => {
-    const req = { 
-        userId: 'authUserId', 
-        name: 'Stamford Bridge',
-        address: 'London',
-        contactNumber: '07747 237 456',
-        pricePerHour: 50  
+    const req = {
+        name: 'Old Trafford',
+        address: 'Manchester',
+        contactNumber: '06646 888 959',
+        pricePerHour: 100  
     };
     const res = await request(app)
         .post('/api/football-pitch')
