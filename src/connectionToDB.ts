832a98c135b588b79b081c7fa5dc004f9462cf85
Noyan Alimov <noyan.alimov@gmail.com>
2020-10-05 19:29:45 +0100

    making use of .env

--- src/connectionToDB.ts
+++ src/connectionToDB.ts
@@ -2,21 +2,23 @@ import { createConnection } from "typeorm";
 import { FootballPitch } from './entities/FootballPitch';
 import { Datee } from './entities/Datee';
 import { HourlyTime } from './entities/HourlyTime';
+import dotenv = require('dotenv');
+dotenv.config();
 
 let database: string;
 
 if (process.env.NODE_ENV === 'test') {
     database = 'footballtest'
 } else {
-    database = 'footballtest'
+    database = `${process.env.DB_NAME}`
 }
 
 export const connectionToDB = createConnection({
     type: "postgres",
-    host: "localhost",
+    host: `${process.env.DB_HOST}`,
     port: 5432,
-    username: "noyan",
-    password: "na040898",
+    username: `${process.env.DB_USERNAME}`,
+    password: `${process.env.DB_PASSWORD}`,
     database,
     entities: [
         FootballPitch,
